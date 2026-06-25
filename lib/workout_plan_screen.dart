137766bde365188e41cf6eb843a1309a6654f63d
Harsh Singh <singhharsh5188@gmail.com>
2026-06-25 18:33:36 +0530

    Added detailed workout plan

--- lib/workout_plan_screen.dart
+++ lib/workout_plan_screen.dart
@@ -10,23 +10,79 @@ class WorkoutPlanScreen extends StatelessWidget {
 
     if (UserDataService.goal.toLowerCase().contains('lose')) {
       workoutPlan = [
-        {'day': 'Monday', 'workout': '30 min Walk + Dumbbell Workout'},
-        {'day': 'Tuesday', 'workout': 'Chest + Triceps'},
-        {'day': 'Wednesday', 'workout': 'Leg Day'},
-        {'day': 'Thursday', 'workout': 'Cardio + Abs'},
-        {'day': 'Friday', 'workout': 'Back + Biceps'},
-        {'day': 'Saturday', 'workout': 'Full Body Workout'},
-        {'day': 'Sunday', 'workout': 'Rest + Stretching'},
+        {
+          'day': 'Monday',
+          'workout':
+              '30 min Fast Walk\n3x15 Squats\n3x12 Dumbbell Press\n3x30 sec Plank'
+        },
+        {
+          'day': 'Tuesday',
+          'workout':
+              '25 min Walk\n3x12 Dumbbell Rows\n3x12 Bicep Curls\n3x20 Mountain Climbers'
+        },
+        {
+          'day': 'Wednesday',
+          'workout':
+              '3x15 Lunges\n3x15 Squats\n3x20 Calf Raises\n15 min Walk'
+        },
+        {
+          'day': 'Thursday',
+          'workout':
+              '20 min Cardio\n3x20 Jumping Jacks\n3x30 sec Plank\n3x15 Crunches'
+        },
+        {
+          'day': 'Friday',
+          'workout':
+              '3x12 Dumbbell Rows\n3x12 Hammer Curls\n3x15 Pushups'
+        },
+        {
+          'day': 'Saturday',
+          'workout':
+              'Full Body Circuit\nSquats\nPushups\nRows\nPlank\nRepeat 3 Rounds'
+        },
+        {
+          'day': 'Sunday',
+          'workout':
+              'Recovery Day\n20 min Walk\nStretching'
+        },
       ];
     } else {
       workoutPlan = [
-        {'day': 'Monday', 'workout': 'Chest'},
-        {'day': 'Tuesday', 'workout': 'Back'},
-        {'day': 'Wednesday', 'workout': 'Legs'},
-        {'day': 'Thursday', 'workout': 'Shoulders'},
-        {'day': 'Friday', 'workout': 'Arms'},
-        {'day': 'Saturday', 'workout': 'Full Body'},
-        {'day': 'Sunday', 'workout': 'Rest'},
+        {
+          'day': 'Monday',
+          'workout':
+              'Chest\n4x12 Dumbbell Press\n4x12 Pushups'
+        },
+        {
+          'day': 'Tuesday',
+          'workout':
+              'Back\n4x12 Rows\n4x12 Deadlifts'
+        },
+        {
+          'day': 'Wednesday',
+          'workout':
+              'Legs\n4x15 Squats\n4x12 Lunges'
+        },
+        {
+          'day': 'Thursday',
+          'workout':
+              'Shoulders\n4x12 Shoulder Press\n4x15 Lateral Raises'
+        },
+        {
+          'day': 'Friday',
+          'workout':
+              'Arms\n4x12 Bicep Curls\n4x12 Tricep Extensions'
+        },
+        {
+          'day': 'Saturday',
+          'workout':
+              'Full Body Workout'
+        },
+        {
+          'day': 'Sunday',
+          'workout':
+              'Rest Day'
+        },
       ];
     }
 
@@ -44,7 +100,9 @@ class WorkoutPlanScreen extends StatelessWidget {
               child: ListTile(
                 leading: const Text('🏋️'),
                 title: Text(workoutPlan[index]['day']!),
-                subtitle: Text(workoutPlan[index]['workout']!),
+                subtitle: Text(
+                  workoutPlan[index]['workout']!,
+                ),
               ),
             );
           },
